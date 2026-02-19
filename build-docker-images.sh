bca08619cd19885253fc7bac68066846495c00bd
Frederik Petersen <frap@itu.dk>
2026-02-19 17:09:39 +0100

    changed image name

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,11 +1,11 @@
 ﻿#!/usr/bin/env bash
 
 echo "Building Minitwit Images"
-docker build -t $DOCKER_USERNAME/minitwitimage -f Dockerfile-MiniTwit .
+docker build -t $DOCKER_USERNAME/minitwitimage-e -f Dockerfile-MiniTwit .
 
 echo "Login to Dockerhub, provide your password below..."
 read -s DOCKER_PASSWORD
 echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "Pushing Minitwit Images to Dockerhub..."
-docker push $DOCKER_USERNAME/minitwitimage:latest
+docker push $DOCKER_USERNAME/minitwitimage-e:latest
