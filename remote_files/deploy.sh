50e362eb7a41a0f6b74cdfe5a40a9bc6e0a0f3e0
Frederik Anker Hørup Petersen <93933920+tropaadigselv@users.noreply.github.com>
2026-04-21 11:59:29 +0200

    Update deploy script to use docker-compose syntax

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 source ~/.bash_profile
 
 cd /minitwit
-
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up -d
+export DOCKER_USERNAME=$DOCKER_USERNAME
+docker-compose -f /minitwit/docker-compose.yml pull
+docker-compose -f /minitwit/docker-compose.yml up -d
