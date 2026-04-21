519aaa968ef15a2f8c7ed54a88a2d0fc67848709
Frederik Anker Hørup Petersen <93933920+tropaadigselv@users.noreply.github.com>
2026-04-21 15:12:04 +0200

    Set Docker username in deploy script
    Add Docker username environment variable for deployment.

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -2,6 +2,7 @@
 source ~/.bash_profile
 
 cd /minitwit
+export DOCKER_USERNAME="gartner1et"
 
 docker compose -f /minitwit/docker-compose.yml pull
 docker compose -f /minitwit/docker-compose.yml up -d
